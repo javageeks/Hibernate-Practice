c57e1dbfba33d77cf3d6ba5221e2bc6243720d8a
pratyoosh.tripathi <pratyoosh.tripathi@yash.com>
2017-12-08 08:06:07 +0530

    adding component mapping code base

---
+++ hibernate-practice/hibernate_mappings_component/src/main/java/com/greenhorn/common/App.java
@@ -0,0 +1,45 @@
+package com.greenhorn.common;
+
+import org.hibernate.Session;
+
+import com.greenhorn.model.Address;
+import com.greenhorn.model.Student;
+import com.greenhorn.persistence.HibernateUtil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	 System.out.println("Maven + Hibernate + MySQL");
+    	 
+         Student student = new Student();  // student instantiation state .....
+         Address presentAdd = new Address();
+         Address permAdd = new Address();
+         Session session = HibernateUtil.getSessionFactory().openSession();
+         session.beginTransaction();
+         presentAdd.setStreet("ABC");
+         presentAdd.setCity("HYD");
+         presentAdd.setState("TS");
+         presentAdd.setZipcode("23566");
+         
+         permAdd.setStreet("XYZ");
+         permAdd.setCity("BANGALORE");
+         permAdd.setState("KA");
+         permAdd.setZipcode("5463667");
+         
+         student.setFirstName("Harish"); // persistent identity -- primary key and java identity 
+         student.setLastName("V");
+         student.setTitle("Mr");
+         student.setPresentAdd(presentAdd);
+         student.setPermAdd(permAdd);
+         session.save(student);
+         session.getTransaction().commit();
+         session.close();
+        
+         
+    }
+}
---
+++ hibernate-practice/hibernate_mappings_component/src/main/java/com/greenhorn/model/Student.java
@@ -0,0 +1,64 @@
+package com.greenhorn.model;
+
+import java.io.Serializable;
+
+public class Student implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String title; //Mr or Miss
+	private String firstName;
+	private String lastName;
+	private Address presentAdd;
+	private Address permAdd;
+	
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", title=" + title + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", presentAdd=" + presentAdd + ", permAdd=" + permAdd + "]";
+	}
+	public Address getPresentAdd() {
+		return presentAdd;
+	}
+	public void setPresentAdd(Address presentAdd) {
+		this.presentAdd = presentAdd;
+	}
+	public Address getPermAdd() {
+		return permAdd;
+	}
+	public void setPermAdd(Address permAdd) {
+		this.permAdd = permAdd;
+	}
+	
+
+}
